ce82bf9ceefefe08b922dfe516638b9fc2c5e3f5
mkulak <kleshney@gmail.com>
2018-05-10 21:37:07 +0200

    ignore rules

--- src/main/kotlin/swali/Linter.kt
+++ src/main/kotlin/swali/Linter.kt
@@ -7,7 +7,9 @@ class Linter(val rules: List<Rule>) {
     fun doLint(request: LintingRequest): LintingResponse =
         try {
             val swagger = Swagger20Parser().parse(request.apiDefinition)
-            val violations = rules.mapNotNull { it.validate(swagger) }
+            val toIgnore = request.ignoreRules.orEmpty().toSet()
+            val rulesToApply = rules.filter { it.id !in toIgnore }
+            val violations = rulesToApply.mapNotNull { it.validate(swagger) }
             violations.toResponse()
         } catch (e: Exception) {
             listOf(invalidApiViolation).toResponse()
