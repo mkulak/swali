b62437c0068a824ce91b4ea0fe9a549bf5cd3b2e
Mykhailo Kulak <mykhailo.kulak@olx.com>
2020-03-28 21:25:32 +0100

    add cf worker (wip)

---
+++ swali-cf/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+external fun addEventListener(type: String, f: (dynamic) -> Unit): String = definedExternally
+external class Request
+external class Response(body: dynamic, init: dynamic)
+
+fun main() {
+    addEventListener("fetch") { event: dynamic ->
+        event.respondWith(handleRequest(event.request))
+    }
+}
+
+fun handleRequest(request: dynamic): dynamic {
+    val response = Response("ABC", mapOf("headers" to mapOf("Content-Type" to "text/html")))
+    return response
+}
+
