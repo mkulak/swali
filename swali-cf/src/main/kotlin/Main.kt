2d3ee37818b9a6ed2849b6950bcef238d71f9ca3
Mykhailo Kulak <mykhailo.kulak@olx.com>
2020-03-29 21:44:44 +0200

    coroutine works

--- swali-cf/src/main/kotlin/Main.kt
+++ swali-cf/src/main/kotlin/Main.kt
@@ -1,15 +1,31 @@
-external fun addEventListener(type: String, f: (dynamic) -> Unit): String = definedExternally
-external class Request
-external class Response(body: dynamic, init: dynamic)
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.asDeferred
+import kotlinx.coroutines.asPromise
+import kotlinx.coroutines.async
+import kotlinx.coroutines.await
+import kotlinx.coroutines.launch
+import org.w3c.fetch.Request
+import org.w3c.fetch.Response
+import org.w3c.fetch.ResponseInit
+import kotlin.js.Promise
 
 fun main() {
-    addEventListener("fetch") { event: dynamic ->
-        event.respondWith(handleRequest(event.request))
+    addEventListener("fetch") { event: FetchEvent ->
+        Promise<Unit> { resolve, reject ->
+            GlobalScope.async(Dispatchers.Unconfined) {
+                event.respondWith(handleRequest(event.request))
+                resolve(Unit)
+            }
+        }
     }
 }
 
-fun handleRequest(request: dynamic): dynamic {
-    val response = Response("Hello from Kotlin", mapOf("headers" to mapOf("Content-Type" to "text/html")))
+fun handleRequest(request: Request): Response {
+    val foo = request.headers.get("Foo")
+//    val body = request.text().await()
+    val url = request.url
+    val response = Response("Deferred Main $url $foo body", ResponseInit(headers = mapOf("Content-Type" to "text/html")))
     return response
 }
 
