f80c7013285c44b4558cd895c04aa3cf011164de
Mykhailo Kulak <mykhailo.kulak@olx.com>
2021-01-03 17:11:50 +0100

    http -> https, more domains

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -4,6 +4,7 @@ import * as gcp from "@pulumi/gcp";
 
 const region = "europe-west3"
 let host = "swali.kvarto.net";
+let certificateHost = "*.kvarto.net";
 
 const staticSiteBucket = new gcp.storage.Bucket("fe-static", {
     name: "fe-static",
@@ -52,18 +53,33 @@ const urlMap = new gcp.compute.URLMap("site-lb", {
     description: "",
     name: 'site-lb',
     defaultService: siteBackend.id,
-    hostRules: [{
-        hosts: [host],
-        pathMatcher: "allpaths",
-    }],
-    pathMatchers: [{
-        name: "allpaths",
-        defaultService: siteBackend.id,
-        pathRules: [{
-            paths: ["/*"],
-            service: siteBackend.id,
-        }],
-    }],
+    hostRules: [
+        {
+            hosts: [host],
+            pathMatcher: "allpaths",
+        },
+        {
+            hosts: ["app.kvarto.net"],
+            pathMatcher: "special",
+        }
+    ],
+    pathMatchers: [
+        {
+            name: "allpaths",
+            defaultService: siteBackend.id,
+            pathRules: [{
+                paths: ["/*"],
+                service: siteBackend.id,
+            }],
+        },
+        {
+            name: "special",
+            defaultUrlRedirect: {
+                hostRedirect: "google.com",
+                stripQuery: true
+            },
+        },
+    ],
 });
 
 const sslCertificate = new gcp.compute.ManagedSslCertificate("swali-certificate", {
@@ -72,22 +88,45 @@ const sslCertificate = new gcp.compute.ManagedSslCertificate("swali-certificate"
     type: "MANAGED",
 });
 
+const appSslCertificate = new gcp.compute.ManagedSslCertificate("app-certificate", {
+    name: "app-certificate",
+    managed: {domains: ["app.kvarto.net"],},
+    type: "MANAGED",
+});
+
 const proxy = new gcp.compute.TargetHttpsProxy("site-lb-target-proxy", {
     name: "site-lb-target-proxy",
     urlMap: urlMap.id,
-    sslCertificates: [sslCertificate.id],
+    sslCertificates: [sslCertificate.id, appSslCertificate.id],
 });
-
 
 const forwardingRule = new gcp.compute.GlobalForwardingRule("site-fe", {
     name: 'site-fe',
     portRange: '443-443',
-    target: proxy.id,
     ipAddress: ipAddress.address,
+    target: proxy.id,
+});
+
+const httpForwardingRule = new gcp.compute.GlobalForwardingRule("http-to-https", {
+    name: 'http-to-https',
+    portRange: '80',
+    ipAddress: ipAddress.address,
+    target: proxy.id,
+});
+const httpUrlMap = new gcp.compute.URLMap("http-map", {
+    name: 'http-map',
+    defaultUrlRedirect: {
+        httpsRedirect: true,
+        stripQuery: false,
+    }
+});
+const httpProxy = new gcp.compute.TargetHttpProxy("http-proxy", {
+    name: "http-proxy",
+    urlMap: httpUrlMap.id,
 });
 
 
 export const bucketName = staticSiteBucket.url;
 export const ip = ipAddress.address;
-export const urlMapId = urlMap.id;
+// export const urlMapId = urlMap.id;
 export const sslCertId = sslCertificate.id;
