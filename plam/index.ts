5511ed380e28356589ae89fe8235010b8cde6709
mkulak <kleshney@gmail.com>
2021-09-10 19:13:46 +0200

    add lambda with docker

--- plam/index.ts
+++ plam/index.ts
@@ -1,11 +1,31 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as awsx from "@pulumi/awsx";
+import * as aws from "@pulumi/aws"
+import * as awsx from "@pulumi/awsx"
+import * as pulumi from "@pulumi/pulumi"
+
+const image = awsx.ecr.buildAndPushImage("sampleapp", {
+    context: "./app",
+})
+const role = new aws.iam.Role("lambdaRole", {
+    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ Service: "lambda.amazonaws.com" }),
+})
+new aws.iam.RolePolicyAttachment("lambdaFullAccess", {
+    role: role.name,
+    policyArn: aws.iam.ManagedPolicy.LambdaFullAccess,
+})
+
+const func = new aws.lambda.Function("helloworld", {
+    packageType: "Image",
+    imageUri: image.imageValue,
+    role: role.arn,
+    timeout: 60,
+})
+
 
 const endpoint = new awsx.apigateway.API("hello", {
     routes: [
         // Serve static files from the `www` folder (using AWS S3)
         {
-            path: "/",
+            path: "/site",
             localPath: "www",
         },
 
@@ -21,8 +41,15 @@ const endpoint = new awsx.apigateway.API("hello", {
                     headers: { "content-type": "application/json" },
                 })
             }
+        },
+
+        {
+            path: "/{route+}",
+            method: "GET",
+            eventHandler: func,
         }
     ]
-});
+})
 
-export const url = endpoint.url;
+export const url = endpoint.url
+export const helloWorldUrl = pulumi.interpolate`${endpoint.url}World`
