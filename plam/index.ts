f02e52d63d808a1d0b2c98c8402b28841ea5166b
mkulak <kleshney@gmail.com>
2021-09-10 15:57:11 +0200

    remove bucket add readme

--- plam/index.ts
+++ plam/index.ts
@@ -1,20 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
-
-const bucket = new aws.s3.Bucket("my-bucket", {
-    website: {
-        indexDocument: "index.html",
-    }
-});
-
-const bucketObject = new aws.s3.BucketObject("index.html", {
-    acl: "public-read",
-    contentType: "text/html",
-    bucket: bucket,
-    source: new pulumi.asset.FileAsset("index.html")
-});
-
 
 const endpoint = new awsx.apigateway.API("hello", {
     routes: [
@@ -40,6 +25,4 @@ const endpoint = new awsx.apigateway.API("hello", {
     ]
 });
 
-export const bucketName = bucket.id;
-export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
 export const url = endpoint.url;
---
+++ plam/readme.md
@@ -0,0 +1,6 @@
+To create the stack:
+```pulumi up```
+
+To call lambda: ```curl $(pulumi output url)/source```
+
+To destroy the stack: ```pulumi destroy```
